956a00e4d4b4a2cbd74898ffbd70c013a097021f
romain-lienard <lienard.romain@hotmail.com>
2020-05-20 13:21:28 +0200

    Gallery 6

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 filterSelection("all") // Execute the function and show all columns
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("column");
+  x = document.getElementsByClassName("thumbnail");
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
