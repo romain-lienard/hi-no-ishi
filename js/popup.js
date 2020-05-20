34e227c7d31ef800e451f5e65424ec5b0d53c8d8
romain-lienard <lienard.romain@hotmail.com>
2020-05-20 14:48:17 +0200

    Gallery 10

--- js/popup.js
+++ js/popup.js
@@ -15,3 +15,15 @@ $(document).ready(function() {
       }
   });
 });
+
+$(document).ready(function() {
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
