c26b82e6470e1a39e9aeeb5c66ae2616a98b2a3a
romain-lienard <lienard.romain@hotmail.com>
2020-05-20 14:08:27 +0200

    Gallery 8

---
+++ js/popup.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.image-link').magnificPopup({
+      type: 'image',
+      mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+      gallery:{
+          enabled:true,
+          zoom: {
+              enabled: true, // By default it's false, so don't forget to enable it
+              duration: 300, // duration of the effect, in milliseconds
+              easing: 'ease-in-out', // CSS transition easing function
+              opener: function(openerElement) {
+                  return openerElement.is('img') ? openerElement : openerElement.find('img');
+              }
+          }
+      }
+  });
+});
